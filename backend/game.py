25f4c135c483ea9ac060a604d3652cd3fde219a9
Piotr Danielczyk <piotrekdanielczyk@gmail.com>
2021-01-09 16:24:04 +0100

    Add optimisation to placing blocks (peak indexes)

--- backend/game.py
+++ backend/game.py
@@ -14,7 +14,8 @@ class Game:
         self.board_height = board.shape[0]
         self.board_width = board.shape[1]
         self.__taken_board = np.zeros((self.board_height, self.board_width), dtype=int)
-        self.__column_peaks = np.zeros((self.board_width,), dtype=int)
+        self.__column_peaks_row_indexes = np.array([self.board_height - 1 for i in range(0, self.board_width)], dtype=int)
+        print(self.__column_peaks_row_indexes)
         self.current_shape = None
 
     def print_shapes(self):
@@ -39,6 +40,15 @@ class Game:
     def get_taken_board(self) -> np.array:
         return self.__taken_board
 
+    """end_col is not inclusive, just like with slicing lists"""
+    def update_column_peaks_row_indexes(self, start_col: int, end_col: int):
+        for col_index, column in enumerate(self.__taken_board.T):
+            if start_col <= col_index < end_col:
+                for row_index, field_value in enumerate(column):
+                    if field_value > 0:       # taken space
+                        self.__column_peaks_row_indexes[col_index] = row_index - 1
+                        break
+
     def is_column_index_correct(self, col_index: int, block: np.array) -> bool:
         block_width = len(block[0])
         if col_index + block_width <= self.board_width:
@@ -52,8 +62,8 @@ class Game:
     def find_start_row(self, start_col: int, block: np.array) -> int:
         block_width = len(block[0])
         block_height = len(block)
-        peaks_of_columns_below_block = self.__column_peaks[start_col:start_col+block_width]
-        initial_start_row_index = self.board_height - max(peaks_of_columns_below_block) - block_height
+        peaks_of_columns_below_block = self.__column_peaks_row_indexes[start_col:start_col+block_width]
+        initial_start_row_index = min(peaks_of_columns_below_block) - block_height + 1
         start_row_index = initial_start_row_index
         while start_row_index >= 0:
             if self.can_place_block(start_row_index, start_col, block):
@@ -80,8 +90,11 @@ class Game:
             return False
 
     def place_block(self, start_row: int, start_col: int, block: np.array):
-        self.__taken_board[start_row:start_row + block.shape[0], start_col:start_col + block.shape[1]] += block
+        end_row = start_row + block.shape[0]            # this row will not be included (exclusive indexing)
+        end_col = start_col + block.shape[1]            # this column will not be included (exclusive indexing)
+        self.__taken_board[start_row:end_row, start_col:end_col] += block
         # TODO add update for self.board ?
+        self.update_column_peaks_row_indexes(start_col, end_col)
 
     def run_game(self):
         print('in run_game')

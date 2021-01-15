f178ce4a203e2233e2872e88b36c10ff6f2a1c58
Piotr Danielczyk <piotrekdanielczyk@gmail.com>
2021-01-15 19:45:51 +0100

    Add miscellaneous tests for boards

--- tests/boards_tests.py
+++ tests/boards_tests.py
@@ -2,10 +2,14 @@ import unittest
 
 import numpy as np
 
-from backend.boards import FieldType, BOARD_1, BOARD_2, BOARD_3, BOARD_4
+from backend.boards import FieldType, BOARD_1, BOARD_2, BOARD_3, BOARD_4, PAIRS_FIELDS
 
 
 class BoardsContainingAllowedFields(unittest.TestCase):
+    """
+    Test if all boards (BOARD_1, BOARD_2, BOARD_3, BOARD_4) contain only fields (specified by FieldType enum) which are
+    valid for this type of board.
+    """
     def test_if_board_1_contains_only_allowed_fields(self):
         allowed_fields_for_board_1 = [FieldType.EMPTY.value, FieldType.EXTRA_EMPTY.value]
         for field in np.nditer(BOARD_1):
@@ -29,6 +33,24 @@ class BoardsContainingAllowedFields(unittest.TestCase):
                                       FieldType.PAIR_5.value]
         for field in np.nditer(BOARD_4):
             self.assertIn(field, allowed_fields_for_board_4)
+
+
+class BoardsMiscellaneousChecks(unittest.TestCase):
+    """
+    Miscellaneous tests for boards (BOARD_1, BOARD_2, BOARD_3, BOARD_4).
+    """
+    def test_if_taken_equals_one(self):
+        self.assertEqual(FieldType.TAKEN.value, 1)
+
+    def test_if_all_pair_fields_in_PAIRS_FIELDS(self):
+        correct_pair_fields = [FieldType.PAIR_1.value, FieldType.PAIR_2.value, FieldType.PAIR_3.value, FieldType.PAIR_4.value, FieldType.PAIR_5.value]
+        self.assertEqual(len(correct_pair_fields), len(PAIRS_FIELDS))
+        self.assertEqual(set(correct_pair_fields), set(PAIRS_FIELDS))
+
+    def test_if_all_boards_are_the_same_size(self):
+        self.assertEqual(BOARD_1.shape, BOARD_2.shape)
+        self.assertEqual(BOARD_2.shape, BOARD_3.shape)
+        self.assertEqual(BOARD_3.shape, BOARD_4.shape)
 
 
 if __name__ == '__main__':

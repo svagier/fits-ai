137e514ef696a5266a2ba13fd9ab8706723875ac
Piotr Danielczyk <piotrekdanielczyk@gmail.com>
2021-01-15 21:59:12 +0100

    Add BoardManager to boards_tests

--- tests/boards_tests.py
+++ tests/boards_tests.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from backend.boards import FieldType, BOARD_1, BOARD_2, BOARD_3, BOARD_4, PAIRS_FIELDS
+from backend.boards import FieldType, PAIRS_FIELDS, BoardManager
 
 
 class BoardsContainingAllowedFields(unittest.TestCase):
@@ -10,28 +10,32 @@ class BoardsContainingAllowedFields(unittest.TestCase):
     Test if all boards (BOARD_1, BOARD_2, BOARD_3, BOARD_4) contain only fields (specified by FieldType enum) which are
     valid for this type of board.
     """
+    def __init__(self, methodName='runTest'):
+        self.__board_manager = BoardManager()
+        super().__init__(methodName)
+
     def test_if_board_1_contains_only_allowed_fields(self):
         allowed_fields_for_board_1 = [FieldType.EMPTY.value, FieldType.EXTRA_EMPTY.value]
-        for field in np.nditer(BOARD_1):
+        for field in np.nditer(self.__board_manager.get_board(1)):
             self.assertIn(field, allowed_fields_for_board_1)
 
     def test_if_board_2_contains_only_allowed_fields(self):
         allowed_fields_for_board_2 = [FieldType.EMPTY.value, FieldType.EXTRA_EMPTY.value, FieldType.PLUS_1.value,
                                       FieldType.PLUS_2.value, FieldType.PLUS_3.value]
-        for field in np.nditer(BOARD_2):
+        for field in np.nditer(self.__board_manager.get_board(2)):
             self.assertIn(field, allowed_fields_for_board_2)
 
     def test_if_board_3_contains_only_allowed_fields(self):
         allowed_fields_for_board_3 = [FieldType.EMPTY.value, FieldType.EXTRA_EMPTY.value, FieldType.PLUS_1.value,
                                       FieldType.PLUS_2.value, FieldType.PLUS_3.value, FieldType.MINUS_5.value]
-        for field in np.nditer(BOARD_3):
+        for field in np.nditer(self.__board_manager.get_board(3)):
             self.assertIn(field, allowed_fields_for_board_3)
 
     def test_if_board_4_contains_only_allowed_fields(self):
         allowed_fields_for_board_4 = [FieldType.EMPTY.value, FieldType.EXTRA_EMPTY.value, FieldType.PAIR_1.value,
                                       FieldType.PAIR_2.value, FieldType.PAIR_3.value, FieldType.PAIR_4.value,
                                       FieldType.PAIR_5.value]
-        for field in np.nditer(BOARD_4):
+        for field in np.nditer(self.__board_manager.get_board(4)):
             self.assertIn(field, allowed_fields_for_board_4)
 
 
@@ -39,6 +43,10 @@ class BoardsMiscellaneousChecks(unittest.TestCase):
     """
     Miscellaneous tests for boards (BOARD_1, BOARD_2, BOARD_3, BOARD_4).
     """
+    def __init__(self, methodName='runTest'):
+        self.__board_manager = BoardManager()
+        super().__init__(methodName)
+
     def test_if_taken_equals_one(self):
         self.assertEqual(FieldType.TAKEN.value, 1)
 
@@ -49,9 +57,9 @@ class BoardsMiscellaneousChecks(unittest.TestCase):
         self.assertEqual(set(correct_pair_fields), set(PAIRS_FIELDS))
 
     def test_if_all_boards_are_the_same_size(self):
-        self.assertEqual(BOARD_1.shape, BOARD_2.shape)
-        self.assertEqual(BOARD_2.shape, BOARD_3.shape)
-        self.assertEqual(BOARD_3.shape, BOARD_4.shape)
+        self.assertEqual(self.__board_manager.get_board(1).shape, self.__board_manager.get_board(2).shape)
+        self.assertEqual(self.__board_manager.get_board(2).shape, self.__board_manager.get_board(3).shape)
+        self.assertEqual(self.__board_manager.get_board(3).shape, self.__board_manager.get_board(4).shape)
 
 
 if __name__ == '__main__':

0a3d2ccad890c9fcdddccba9afcc46f34e84cec0
Piotr Danielczyk <piotrekdanielczyk@gmail.com>
2021-01-16 00:05:36 +0100

    Add check for mirrored rotations to the test

--- tests/shapes_tests.py
+++ tests/shapes_tests.py
@@ -32,19 +32,23 @@ class ShapesCorrectnessTestCase(unittest.TestCase):
             for shape_rotation in all_rotations_of_shape:
                 self.assertEqual(number_of_ones_in_shape, int(np.sum(shape_rotation)))
 
-    def test_if_each_shape_includes_all_rotations(self):
+    def test_if_each_shape_includes_all_rotations_and_all_mirrored_versions(self):
+        """Test if list of all rotations for each shape contains each possible rotation of this shape, including all
+        possible rotations for mirrored shape (flipped)."""
         for all_rotations_of_shape in self.__shapes_manager.get_all_shapes_dict().values():
             first_rotation_of_shape = all_rotations_of_shape[0]
-            for i in range(1, 4):
-                rotated_shape = np.rot90(first_rotation_of_shape, k=i, axes=(0, 1))
-                contained = False
-                for rotation_array in all_rotations_of_shape:
-                    if rotation_array.shape == rotated_shape.shape:
-                        comparison = rotation_array == rotated_shape
-                        if comparison.all():
-                            contained = True
-                            break
-                self.assertTrue(contained)
+            flipped_first_rotation_of_shape = np.flip(first_rotation_of_shape)
+            for shape in [first_rotation_of_shape, flipped_first_rotation_of_shape]:
+                for i in range(1, 4):       # rotation for 90, 180 and 270 degrees
+                    rotated_shape = np.rot90(shape, k=i, axes=(0, 1))
+                    contained = False
+                    for rotation_array in all_rotations_of_shape:
+                        if rotation_array.shape == rotated_shape.shape:
+                            comparison = rotation_array == rotated_shape
+                            if comparison.all():
+                                contained = True
+                                break
+                    self.assertTrue(contained)
 
 
 if __name__ == '__main__':
